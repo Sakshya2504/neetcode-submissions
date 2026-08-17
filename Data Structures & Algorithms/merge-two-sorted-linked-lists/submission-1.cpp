29e43b40a21540134d6a9999bff38992ec047061
Sakshya2504 <singhkaserasakshya2504@gmail.com>
2026-08-17 18:24:03 +0530

    Add: merge-two-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-1.cpp
@@ -0,0 +1,80 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+
+        if (list1 == nullptr) return list2;
+        if (list2 == nullptr) return list1;
+
+        ListNode* head = nullptr;
+        ListNode* curr = nullptr;
+
+        if (list1->val <= list2->val) {
+            head = list1;
+            list1 = list1->next;
+        }
+        else {
+            head = list2;
+            list2 = list2->next;
+        }
+
+        curr = head;
+
+        while (list1 != nullptr && list2 != nullptr) {
+
+            if (list1->val <= list2->val) {
+                curr->next = list1;
+                list1 = list1->next;
+            }
+            else {
+                curr->next = list2;
+                list2 = list2->next;
+            }
+
+            curr = curr->next;
+        }
+
+        if (list1 != nullptr)
+            curr->next = list1;
+
+        if (list2 != nullptr)
+            curr->next = list2;
+
+        return head;
+    }
+
+    ListNode* merge(vector<ListNode*>& lists, int left, int right) {
+
+        if (left > right)
+            return nullptr;
+
+        if (left == right)
+            return lists[left];
+
+        int mid = left + (right - left) / 2;
+
+        ListNode* leftList = merge(lists, left, mid);
+        ListNode* rightList = merge(lists, mid + 1, right);
+
+        return mergeTwoLists(leftList, rightList);
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+
+        if (lists.empty())
+            return nullptr;
+
+        return merge(lists, 0, lists.size() - 1);
+    }
+};
